97d0890fcfde9dd399b36267a4090f9a6e50b51c
_oraklet <olle_wikstrom@hotmail.com>
2024-02-21 17:55:07 +0100

    Added create to bun sqlite

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,5 +1,5 @@
 import { drizzle } from 'drizzle-orm/bun-sqlite';
 import { Database } from 'bun:sqlite';
 
-const sqlite = new Database('db/drizzle.db');
+const sqlite = new Database('db/drizzle.db', {create: true});
 export const db = drizzle(sqlite);
